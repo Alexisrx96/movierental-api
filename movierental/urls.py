93ca224b00848d878023a5469ce5856f7c60f1fe
Irvin Torres <irvinrexx@gmail.com>
2021-12-04 15:57:53 -0600

    Add public endpoints
    Added endpoints for non-admin users (customers)

--- movierental/urls.py
+++ movierental/urls.py
@@ -13,11 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
     path('api/customers/', include('customers.api.urls')),
     path('api/casts/', include('casts.api.urls')),
     path('api/films/', include('films.api.urls')),
+    path('api/', include('public.api.urls')),
 ]
--- public/api/views.py
+++ public/api/views.py
@@ -1,2 +1,36 @@
 from rest_framework import generics
 from rest_framework import mixins
+
+from public.api.serializers import FilmRentSerializer, FilmReturnSerializer
+from customers.api.serializers import UserSerializer
+from customers.models import Rent, User
+
+
+class UserListAPIView(generics.CreateAPIView):
+    permission_classes = []
+    authentication_classes = []
+    queryset = User.objects.filter(is_staff=False).all()
+    serializer_class = UserSerializer
+
+
+class FilmReturnAPIView(
+        mixins.UpdateModelMixin,
+        generics.RetrieveAPIView
+        ):
+    permission_classes = []
+    authentication_classes = []
+    queryset = Rent.objects.all()
+    serializer_class = FilmReturnSerializer
+
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+    def patch(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+
+class FilmRentAPIView(generics.CreateAPIView):
+    permission_classes = []
+    authentication_classes = []
+    queryset = Rent.objects.all()
+    serializer_class = FilmRentSerializer
