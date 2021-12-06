34d19466ef86160ee8a7aad73f8acd31805eccda
Irvin Torres <irvinrexx@gmail.com>
2021-12-05 19:17:03 -0600

    Refactor CastTest
    Test from database were changed to api endpoints

--- casts/tests.py
+++ casts/tests.py
@@ -1,22 +1,24 @@
-from django.db.utils import IntegrityError
-from django.test import TestCase
 from django.utils import timezone
+from rest_framework.reverse import reverse as api_reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-from casts.models import CastMember
 
-
-class CastTestCase(TestCase):
+class CastTestCase(APITestCase):
     def test_cast_birth_day(self):
-        tomorrow = timezone.now() + timezone.timedelta(days=1)
-        yesterday = timezone.now() - timezone.timedelta(days=1)
-        sarah = CastMember(
-            name="Juan Perez",
-            birth_date=yesterday
-        )
-        john = CastMember(
-            name="John Connor",
-            birth_date=tomorrow
-        )
-        sarah.save()
-        with self.assertRaises(IntegrityError):
-            john.save()
+        url = api_reverse('casts:members')
+        after = timezone.now() + timezone.timedelta(days=1)
+        before = timezone.now() - timezone.timedelta(days=10)
+        data = {
+            'name': "Alex",
+            'birth_date': after.date()
+        }
+        response = self.client.post(url, data, format='json')
+        self.assertEquals(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+        data = {
+            'name': "Alex",
+            'birth_date': before.date()
+        }
+        response = self.client.post(url, data, format='json')
+        self.assertEquals(response.status_code, status.HTTP_201_CREATED)
